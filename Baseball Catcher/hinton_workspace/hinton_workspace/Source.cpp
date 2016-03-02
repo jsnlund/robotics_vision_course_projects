528fbe52e7252917807415a22fb28e2e3af03707
Michael Hinton <michael.g.hinton@gmail.com>
2016-03-01 20:45:42 -0700

    Baseball: Got corner detection working on diff threshold image

--- Baseball Catcher/hinton_workspace/hinton_workspace/Source.cpp
+++ Baseball Catcher/hinton_workspace/hinton_workspace/Source.cpp
@@ -42,12 +42,16 @@ int main(int argc, char const *argv[]) {
     String current_image_file_right;
 
 
+    // A vector to hold an array of corner coordinates
+    vector<Point2f> corners_left, corners_right;
+
+
     // Load the first image
     int i = MIN_IMAGE_NUMBER;
     current_image_file_left = FOLDER + IMAGE_PREFIX_LEFT + helper + to_string(MIN_IMAGE_NUMBER) + IMAGE_FILE_SUFFIX;
     current_image_file_right = FOLDER + IMAGE_PREFIX_RIGHT + helper + to_string(MIN_IMAGE_NUMBER) + IMAGE_FILE_SUFFIX;
-    frame_left_prev = imread(current_image_file_left, CV_LOAD_IMAGE_COLOR);
-    frame_right_prev = imread(current_image_file_right, CV_LOAD_IMAGE_COLOR);
+    frame_left_prev = imread(current_image_file_left, CV_LOAD_IMAGE_GRAYSCALE);
+    frame_right_prev = imread(current_image_file_right, CV_LOAD_IMAGE_GRAYSCALE);
     i++;
 
     // for (int i = MIN_IMAGE_NUMBER; i <= MAX_IMAGE_NUMBER; ++i) {
@@ -65,8 +69,8 @@ int main(int argc, char const *argv[]) {
         current_image_file_right = FOLDER + IMAGE_PREFIX_RIGHT + helper + to_string(i) + IMAGE_FILE_SUFFIX;
 
         // Load the image
-        frame_left = imread(current_image_file_left, CV_LOAD_IMAGE_COLOR);
-        frame_right = imread(current_image_file_right, CV_LOAD_IMAGE_COLOR);
+        frame_left = imread(current_image_file_left, CV_LOAD_IMAGE_GRAYSCALE);
+        frame_right = imread(current_image_file_right, CV_LOAD_IMAGE_GRAYSCALE);
 
         // Exit if no images were grabbed
         if( frame_left.empty() ) {
@@ -84,24 +88,44 @@ int main(int argc, char const *argv[]) {
             return -1;
         }
 
-        // cvtColor(frame_display_left, frame_display_left, COLOR_BGR2GRAY);
-        // cvtColor(frame_display_right, frame_display_right, COLOR_BGR2GRAY);
-
+        //
         //// Process the image
-
+        //
 
         // Absolute difference
         absdiff(frame_left, frame_left_prev, frame_left_diff);
         absdiff(frame_right, frame_right_prev, frame_right_diff);
 
-
+        // Try other blurring factors and sizes
+        // Sizes must be an odd number
+        // Try Size(31,31), 15, 15 to get rid of curtain movement
         GaussianBlur(frame_left_diff, frame_left_thresh, Size(7,7), 3.0, 3.0);
         GaussianBlur(frame_right_diff, frame_right_thresh, Size(7,7), 3.0, 3.0);
 
-        threshold(frame_left_thresh,frame_left_thresh,5,256,THRESH_BINARY);
-        threshold(frame_left_thresh,frame_right_thresh,5,256,THRESH_BINARY);
+        threshold(frame_left_thresh, frame_left_thresh, 5, 256, THRESH_BINARY);
+        threshold(frame_right_thresh, frame_right_thresh, 5, 256, THRESH_BINARY);
+        // adaptiveThreshold(frame_right_diff, frame_right_thresh, 5, ADAPTIVE_THRESH_GAUSSIAN_C, THRESH_BINARY, 7, 0.0);
 
+        // TODO: Try out corner detection
+        corners_left.clear();
+        corners_right.clear();
+        // TODO: Apply ROI
+        // goodFeaturesToTrack(frame_left_thresh(Rect(0,0,200,200)), corners_left, 10, 0.01, 10);
+        // goodFeaturesToTrack(frame_right_thresh(Rect(0,0,200,200)), corners_right, 10, 0.01, 10);
+        goodFeaturesToTrack(frame_left_thresh, corners_left, 10, 0.01, 10);
+        goodFeaturesToTrack(frame_right_thresh, corners_right, 10, 0.01, 10);
+        // cornerSubPix(frame_left_thresh, corners_left, Size(5,5), Size(-1,-1), TermCriteria( CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 40, 0.001 ) );
+        // cornerSubPix(frame_right_thresh, corners_right, Size(5,5), Size(-1,-1), TermCriteria( CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 40, 0.001 ) );
 
+        cvtColor(frame_left_thresh, frame_left_thresh, COLOR_GRAY2BGR);
+        cvtColor(frame_right_thresh, frame_right_thresh, COLOR_GRAY2BGR);
+        // Draw detected corners
+        for (int i = 0; i < corners_left.size(); ++i){
+            circle(frame_left_thresh, Point(corners_left[i].x,corners_left[i].y), 20, Scalar(200,80,0), 3);
+        }
+        for (int i = 0; i < corners_right.size(); ++i){
+            circle(frame_right_thresh, Point(corners_right[i].x,corners_right[i].y), 20, Scalar(100,180,80), 3);
+        }
 
         // Show the image output for a sanity check
         imshow("Left", frame_left);
