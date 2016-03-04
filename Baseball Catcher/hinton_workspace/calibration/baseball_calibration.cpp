0ff51c78e167ed16e7c7e66776b21e73c3a2f5a3
Michael Hinton <michael.g.hinton@gmail.com>
2016-03-04 11:14:03 -0700

    Baseball: Got right camera calibrating; Checked values for accuracy

--- Baseball Catcher/hinton_workspace/calibration/baseball_calibration.cpp
+++ Baseball Catcher/hinton_workspace/calibration/baseball_calibration.cpp
@@ -183,13 +183,10 @@ int main(int argc, char const *argv[]){
 
     // There should be equal amounts of elements
     CV_Assert(object_points_vector_left.size() == image_points_vector_left.size());
-    cout << "Calibrating camera with " << object_points_vector_left.size() << " valid images out of a total of " << IMAGE_COUNT_LEFT << endl;
+    cout << "Calibrating LEFT camera with " << object_points_vector_left.size() << " valid images out of a total of " << IMAGE_COUNT_LEFT << endl;
 
     // Calibrate the LEFT camera with the extracted points
     calibrateCamera(object_points_vector_left, image_points_vector_left, frame_display.size(), camera_matrix_left, dist_coeffs_left, rvecs, tvecs);
-
-    // // Clear for the right side
-    // object_points_vector.clear();
 
 
     //
@@ -197,34 +194,69 @@ int main(int argc, char const *argv[]){
     //
 
 
+    // Loop through each calibration image and extract a list of image points
+    for (int i = CALIB_RIGHT_MIN_NUMBER; i <= CALIB_RIGHT_MAX_NUMBER; ++i) {
+        // Load the image
+        calib_image_file_right.str("");
+        calib_image_file_right.clear();
+        calib_image_file_right << FOLDER_CALIB_RIGHT << FILE_PREFIX_CALIB_RIGHT << setw(2) << setfill('0') << to_string(i) << IMAGE_FILE_SUFFIX;
+        frame = imread(calib_image_file_right.str(), CV_LOAD_IMAGE_COLOR);
 
-    // // CV_LOAD_IMAGE_COLOR CV_LOAD_IMAGE_COLOR
-    // for (int i = CALIB_RIGHT_MIN_NUMBER; i <= CALIB_RIGHT_MAX_NUMBER; ++i) {
-    //     // Load the image
-    //     calib_image_file_right << FOLDER_CALIB_RIGHT << FILE_PREFIX_CALIB_RIGHT << setw(2) << setfill('0') << to_string(MIN_IMAGE_NUMBER) << IMAGE_FILE_SUFFIX;
-    //     frame = imread(calib_image_file_right.str(), CV_LOAD_IMAGE_COLOR);
-    //     // Clear corner values, so that it doesn't grow
-    //     image_points.clear();
+        // Exit if no images were grabbed
+        if( frame.empty() ) {
+            cout <<  "Could not open or find the image. Did you unzip the calibration images?" << std::endl ;
+            // Show the image file path
+            cout << "Image File: " << calib_image_file_right.str() << endl;
+            return -1;
+        }
 
-    //     // Exit if no images were grabbed
-    //     if( frame.empty() ) {
-    //         cout <<  "Could not open or find the image. Did you unzip the calibration images?" << std::endl ;
-    //         // Show the image file path
-    //         cout << "Image File: " << calib_image_file_right.str() << endl;
-    //         return -1;
-    //     }
+        //  Get a grayscale copy to work on
+        frame.copyTo(frame_display);
+        cvtColor(frame_display, frame_display, COLOR_BGR2GRAY);
 
-    //     //  Get a grayscale copy to work on
-    //     frame.copyTo(frame_display);
-    //     cvtColor(frame_display, frame_display, COLOR_BGR2GRAY);
+        // Clear corner values, so that it doesn't grow
+        image_points_right.clear();
+
+        //// Process the image
+        pattern_was_found = findChessboardCorners(frame_display, PATTERN_SIZE, chessboard_corners, FIND_CHESSBOARD_FLAGS);
+        if(!pattern_was_found){
+            cout << "Chessboard Corners not found!!" << endl;
+            cout << "Image File: " << calib_image_file_right.str() << endl;
+            // if chessboard is not found, omit the image
+            continue;
+        }
+
+        cornerSubPix(frame_display, chessboard_corners, Size(11,11), Size(-1,-1), TermCriteria( CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1 ));
+
+        // Make sure that we have all the corners we expect...
+        CV_Assert(chessboard_corners.size() == CHESSBOARD_CORNERS_COUNT);
+
+        // Extract all the 2-d corner points into image_point_rights
+        for (int i = 0; i < chessboard_corners.size(); ++i) {
+            image_points_right.push_back(chessboard_corners[i]);
+        }
+        // cout << image_points_right << endl_right;
+
+        // Save the 2-d and 3-d vectors to a vector of vectors
+        object_points_vector_right.push_back(object_points);
+        image_points_vector_right.push_back(image_points_right);
+    }
 
 
-    // }
 
+    // Initialize a 3x3 matrix that will be overwritten (64F is a double, not a float)
+    Mat camera_matrix_right = Mat::zeros(3, 3, CV_64F);
+    // Initialize 8x1 coefficient matrix (64F is a double, not a float)
+    Mat dist_coeffs_right = Mat::zeros(8, 1, CV_64F);
+    // // Throwaway values - we don't care about rvec and tvec until stereo
+    // vector<Mat> rvecs, tvecs;
 
+    // There should be equal amounts of elements
+    CV_Assert(object_points_vector_right.size() == image_points_vector_right.size());
+    cout << "Calibrating RIGHT camera with " << object_points_vector_right.size() << " valid images out of a total of " << IMAGE_COUNT_RIGHT << endl;
 
-
-
+    // Calibrate the LEFT camera with the extracted points
+    calibrateCamera(object_points_vector_right, image_points_vector_right, frame_display.size(), camera_matrix_right, dist_coeffs_right, rvecs, tvecs);
 
 
     //
@@ -267,8 +299,8 @@ int main(int argc, char const *argv[]){
     baseball_params << "intrinsic_left" << camera_matrix_left;
     baseball_params << "distortion_left" << dist_coeffs_left;
     // // Right camera calibration
-    // baseball_params << "intrinsic_right" << camera_matrix_right;
-    // baseball_params << "distortion_right" << dist_coeffs_right;
+    baseball_params << "intrinsic_right" << camera_matrix_right;
+    baseball_params << "distortion_right" << dist_coeffs_right;
     // // Stereo calibration
     // baseball_params << "fundamental" << fundamental_matrix;
     // baseball_params << "translation" << tvec;
