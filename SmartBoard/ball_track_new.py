59217cc126258039cb6e77a08def724b870c2d2c
Jason Lund <jason@Jasons-MBP.app.byu.edu>
2016-04-06 13:04:30 -0600

    Draw and erase using a green object.  Having a hard time determining limits for other colored markers.

---
+++ SmartBoard/ball_track_new.py
@@ -0,0 +1,162 @@
+##################################
+# ----- Ball Tracking Demo ----- #
+##################################
+from collections import deque
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+
+# # construct the argument parse and parse the arguments
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-v", "--video",
+# 	help="path to the (optional) video file")
+# ap.add_argument("-b", "--buffer", type=int, default=64,
+# 	help="max buffer size")
+# args = vars(ap.parse_args())
+
+# def argument_parser():
+# 	parser = argparse.ArgumentParser(description="Change color space of the \
+# 			input video stream using keyboard controls. The control keys are: \
+# 			Binary - 'b', Adaptive Binary - 'g', Canny - 'y', Corners - 'c'  \
+# 			Lines - 'l', Differencing - 'd'")
+# 	return parser
+
+# # define the lower and upper boundaries of the "green"
+# # ball in the HSV color space, then initialize the
+# # list of tracked points
+# greenLower = (35, 60, 50) # (80,60,140)#
+# greenUpper = (64, 255, 255) # (130,100,200)#
+# pts = deque(maxlen=args["buffer"])
+# blurme = True
+
+# # if a video path was not supplied, grab the reference
+# # to the webcam
+# if not args.get("video", False):
+# 	camera = cv2.VideoCapture(0)
+
+# # otherwise, grab a reference to the video file
+# else:
+# 	camera = cv2.VideoCapture(args["video"])
+
+# (grabbed, frame) = camera.read()
+# ink = np.copy(frame)
+# ink[:,:,:] = 0
+# # width = camera.get(cv2.CAP_PROP_FRAME_WIDTH)
+# # height = camera.get(cv2.CAP_PROP_FRAME_HEIGHT)
+# # myType = camera.get(cv2.CAP_PROP_FORMAT)
+# # ink = np.zeros((height,width,3))
+
+# # print(width, height,flush = True)
+# # keep looping
+
+# cv2.namedWindow("Frame", cv2.WND_PROP_FULLSCREEN)
+# cv2.setWindowProperty("Frame", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
+
+
+# # ----- initialize current and previous charcter variables ----- #
+# cur_char = -1
+# prev_char = -1
+
+# while True:
+# 	# grab the current frame
+# 	(grabbed, frame) = camera.read()
+# 	# if first:
+# 	# 	ink = np.copy(frame)
+# 	# 	ink[:,:,:] = 0
+# 	# 	first = False
+# 	# else:
+# 	# 	continue
+# 	# if we are viewing a video and we did not grab a frame,
+# 	# then we have reached the end of the video
+# 	c = cv2.waitKey(1)
+
+# 	# ----- Press ESC to exit program ----- #
+# 	if c == 27:
+# 		break
+
+# 	# ----- reads in keypress characters ----- #
+# 	if c > -1 and c != prev_char:
+# 		cur_char = c
+# 		first = True
+# 	else:
+# 		first = False
+# 	prev_char = c
+
+# 	if args.get("video") and not grabbed:
+# 		break
+ 
+# 	# resize the frame, blur it, and convert it to the HSV
+# 	# color space
+# 	# frame = imutils.resize(frame, width=600)
+# 	if blurme:
+# 		blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+# 	else:
+# 		blurred = frame
+# 	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+ 
+# 	# construct a mask for the color "green", then perform
+# 	# a series of dilations and erosions to remove any small
+# 	# blobs left in the mask
+# 	mask = cv2.inRange(hsv, greenLower, greenUpper)
+# 	mask = cv2.erode(mask, None, iterations=2)
+# 	mask = cv2.dilate(mask, None, iterations=2)
+# 	# find contours in the mask and initialize the current
+# 	# (x, y) center of the ball
+# 	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+# 		cv2.CHAIN_APPROX_SIMPLE)[-2]
+# 	center = None
+ 
+# 	# only proceed if at least one contour was found
+# 	if len(cnts) > 0:
+# 		# find the largest contour in the mask, then use
+# 		# it to compute the minimum enclosing circle and
+# 		# centroid
+# 		c = max(cnts, key=cv2.contourArea)
+# 		((x, y), radius) = cv2.minEnclosingCircle(c)
+# 		M = cv2.moments(c)
+# 		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+ 
+# 		# only proceed if the radius meets a minimum size
+# 		# if radius > 10 and radius < 50:
+# 			# draw the circle and centroid on the frame,
+# 			# then update the list of tracked points
+			
+# 		cv2.circle(frame, center, 5, (255, 0, 0), -1)
+# 		if cur_char == ord('d'):
+# 			cv2.circle(frame, center, int(radius), (200, 200, 0), 2)
+# 			cv2.circle(ink, center, 5, (0, 255, 0), -1)
+# 		if cur_char == ord('e'):
+# 			cv2.circle(frame, center, 30,(0, 100, 255), 2)
+# 			cv2.circle(ink, center, 30, (0, 0, 0), -1)
+
+ 
+# 			# update the points queue
+# 			# pts.appendleft(center)
+# 			# loop over the set of tracked points
+# 	# for i in range(1, len(pts)):
+# 	# 	# if either of the tracked points are None, ignore
+# 	# 	# them
+# 	# 	if pts[i - 1] is None or pts[i] is None:
+# 	# 		continue
+ 
+# 	# 	# otherwise, compute the thickness of the line and
+# 	# 	# draw the connecting lines
+# 	# 	thickness = 2 #int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+# 	# 	cv2.line(ink, pts[i - 1], pts[i], (0, 0, 255), thickness)
+ 
+# 	# show the frame to our screen
+# 	# print(np.mean(frame[:,:,0]), np.mean(frame[:,:,1]), np.mean(frame[:,:,2]), flush = True)
+# 	mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+# 	frame = cv2.add(frame,ink)
+# 	cv2.imshow("Frame", cv2.flip(frame,1))
+# 	# key = cv2.waitKey(1) & 0xFF
+ 
+# 	# # if the 'q' key is pressed, stop the loop
+# 	# if key == ord("q"):
+# 	# 	break
+ 
+# # cleanup the camera and close any open windows
+# camera.release()
+# cv2.destroyAllWindows()
---
+++ SmartBoard/smartboard_c.py
@@ -0,0 +1,163 @@
+##################################
+# ----- SmartBoard Project ----- #
+##################################
+# version b
+
+####################################################################
+####################################################################
+print("importing packages", flush = True)
+import argparse
+import os
+import numpy as np
+import cv2
+import glob
+from matplotlib import pyplot as plt
+
+####################################################################
+####################################################################
+print("defining functions",flush = True)
+def argument_parser():
+	parser = argparse.ArgumentParser(description="Change color space of the \
+			input video stream using keyboard controls. The control keys are: \
+			Binary - 'b', Adaptive Binary - 'g', Canny - 'y', Corners - 'c'  \
+			Lines - 'l', Differencing - 'd'")
+	return parser
+
+def draw(img, corners, imgpts):
+	imgpts = np.int32(imgpts).reshape(-1,2)
+
+	# draw ground floor in green
+	img = cv2.drawContours(img, [imgpts[:4]],-1,(0,255,0),-3)
+
+	# draw pillars in blue color
+	for i,j in zip(range(4),range(4,8)):
+		img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255),3)
+
+	# draw top layer in red color
+	img = cv2.drawContours(img, [imgpts[4:]],-1,(0,0,255),3)
+
+	return img
+
+
+def draw2(img, corners, imgpts):
+	imgpts = np.int32(imgpts).reshape(-1,2)
+
+	# draw ground floor in green
+	img = cv2.drawContours(img, [imgpts[:4]],-1,(0,255,0),-3)
+
+	# draw pillars in blue color
+	for i,j in zip(range(4),range(4,8)):
+		img = cv2.line(img, tuple(imgpts[i]), tuple(imgpts[j]),(255),3)
+
+	# draw top layer in red color
+	img = cv2.drawContours(img, [imgpts[4:]],-1,(0,0,255),3)
+
+	return img
+
+####################################################################
+####################################################################
+print("defining parameters",flush = True)
+title = "Webcam"
+option = "Webcam"
+font = cv2.FONT_HERSHEY_SIMPLEX
+text_col = (255,0,0)
+width_chess = 8
+height_chess = 6
+
+scale_img = 1 # used in the resize function for displaying the video
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+objp = np.zeros((width_chess*height_chess,3), np.float32)
+objp[:,:2] = np.mgrid[0:width_chess,0:height_chess].T.reshape(-1,2)
+# axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
+axis = np.float32([[0,0,0], [0,1,0], [1,1,0], [1,0,0], [0,0,-5],[0,1,-5],[1,1,-5],[1,0,-5] ])
+
+####################################################################
+####################################################################
+print("importing camera parameters", flush = True)
+with np.load('WebcamParams.npz') as X:
+	mtx, dist = [X[i] for i in ('mtx','dist')]#,'rvecs','tvecs')]
+
+####################################################################
+####################################################################
+print("capturing webcam",flush = True)
+cap = cv2.VideoCapture(0)
+ret, frame = cap.read()
+frame = cv2.resize(frame, None, fx=scale_img, fy=scale_img, interpolation=cv2.INTER_AREA)
+rows, cols, rgb = frame.shape
+
+
+####################################################################
+####################################################################
+print("looping...",flush = True)
+if __name__=='__main__':
+	args = argument_parser().parse_args()
+	cap = cv2.VideoCapture(0)
+
+	# ----- Check if the webcam is opened correctly ----- #
+	if not cap.isOpened():
+		raise IOError("Cannot open webcam")
+	else: 
+		print("Webcam Initialized")
+
+	# ----- initialize current and previous charcter variables ----- #
+	cur_char = -1
+	prev_char = -1
+
+	while True:
+		# ----- Read the current frame from webcam ----- #
+		ret, frame = cap.read()
+
+		# ----- Resize the captured image ----- #
+		frame = cv2.resize(frame, None, fx=scale_img, fy=scale_img, interpolation=cv2.INTER_AREA)
+		rows, cols, rgb = frame.shape
+		# wait 1 ms for keypress
+		c = cv2.waitKey(1)
+
+		# ----- Press ESC to exit program ----- #
+		if c == 27:
+			break
+
+		# ----- reads in keypress characters ----- #
+		if c > -1 and c != prev_char:
+			cur_char = c
+			first = True
+		else:
+			first = False
+		prev_char = c
+
+		if cur_char == ord('c'):
+			option = "calibrate"
+			text_col = (255,0,0)
+			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+			ret, corners = cv2.findChessboardCorners(gray, (width_chess,height_chess),None)
+			# print("Corners: ", corners.shape,flush = True)
+			if ret:
+				# print("found Chessboard", flush = True)
+				corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+				# print("Corners2: ", corners2.shape,flush = True)
+				# Find the rotation and translation vectors.
+				retval, rvecs, tvecs = cv2.solvePnP(objp, corners2, mtx, dist)
+				# rvecs, tvecs, inliers = cv2.solvePnPRansac(objp, corners2, mtx, dist)
+
+				# project 3D points to image plane
+				imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, mtx, dist)
+				output = draw(frame,corners2,imgpts)
+			else:
+				output = frame
+
+		elif cur_char == ord('c'):
+			option = "DRAW"
+			text_col = (0,255,0)
+
+
+		else:
+			option = "Webcam"
+			text_col = (255,0,0)
+			output = frame
+
+		cv2.putText(output,option,(10,rows-25), font, 1,text_col,2,cv2.LINE_AA)
+		cv2.imshow(title, output)
+
+	cap.release()
+	cv2.destroyAllWindows()
