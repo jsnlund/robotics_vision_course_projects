68724320ee73937abf2b045e2752d9d8105632bb
Michael Hinton <michael.g.hinton@gmail.com>
2016-04-09 17:13:40 -0600

    Smartboard: Added some guidelines to the headerfiles, so I don't forget

--- SmartBoard/Smartboard/Smartboard/calibration.h
+++ SmartBoard/Smartboard/Smartboard/calibration.h
@@ -1,6 +1,13 @@
-// calibration.h will contain global variables and functions for calibration.cpp
 #pragma once
 
-// Declare things in header files, and define things (initialize) in cpp files
+//
+// Header file guidlines:
+//
+// Only expose variables and functions in the header file that you want other files to see.
+// You don't need to define every global variable or function in the header file that is in the cpp file.
+// Declare things in header files, and define things (initialize) in cpp files.
+// When defining constants, use "extern const" in the header without defining it, and then define/implement it in the cpp file.
+// Do NOT use "using namespace" in headers - only in cpp files.
+// Do NOT include cpp files in headers.
 
 cv::Mat calibrate_smartboard(cv::Mat *frame_camera, cv::Mat *frame_projector, bool calculate_transform);
--- SmartBoard/Smartboard/Smartboard/globals.h
+++ SmartBoard/Smartboard/Smartboard/globals.h
@@ -1,6 +1,17 @@
 // Globals will contain useful globals used in many files across the app
 #pragma once
 
+//
+// Header file guidlines:
+//
+// Only expose variables and functions in the header file that you want other files to see.
+// You don't need to define every global variable or function in the header file that is in the cpp file.
+// Declare things in header files, and define things (initialize) in cpp files.
+// When defining constants, use "extern const" in the header without defining it, and then define/implement it in the cpp file.
+// Do NOT use "using namespace" in headers - only in cpp files.
+// Do NOT include cpp files in headers.
+
+
 // Input image dimensions
 extern const int IMAGE_WIDTH;
 extern const int IMAGE_HEIGHT;
