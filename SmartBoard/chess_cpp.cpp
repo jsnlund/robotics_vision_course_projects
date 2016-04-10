a0deac4a4f1dc91100125f0ac9a2bf312fffbea2
Jason Lund <jason@Jasons-MBP.home>
2016-04-09 21:15:31 -0600

    modified to have 1 square-width of white border.

--- SmartBoard/chess_cpp.cpp
+++ SmartBoard/chess_cpp.cpp
@@ -1,7 +1,7 @@
 //create empty mat same size as chess
 
 Mat frame_calibration = Mat(CALIBRATION_FRAME_SIZE, CV_8UC3);
-frame_calibration.setTo(BLACK);
+frame_calibration.setTo(WHITE);
 
 // int rows = frame_calibration.rows;
 // int cols = frame_calibration.cols;
@@ -16,15 +16,16 @@ int cols = 16;
 int sq = width/cols;
 int sq2 = sq - 1;
 int s;
+// int border = sq2
 
-for (int i = 0; i < rows; ++i){
+for (int i = 1; i < rows-1; ++i){
 	if(i % 2 == 0){ s = 0; }
 	else{ s = 1; }
-	for (int j = 0; j < cols; ++j){
+	for (int j = 1; j < cols-1; ++j){
 		if((j-s) % 2 == 0){
 			int x = j*sq
 			int y = i*sq
-			rectangle(chess, Point(x,y), Point(x+sq2,y+sq2), WHITE, CV_FILLED)//, int lineType=8, int shift=0)
+			rectangle(chess, Point(x,y), Point(x+sq2,y+sq2), BLACK, CV_FILLED)//, int lineType=8, int shift=0)
 		}
 	}
 }
